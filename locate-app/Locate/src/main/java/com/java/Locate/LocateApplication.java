8aeae84479aa026108dec1f5f9762083677062a1
edgar288 <edgarlucero288@gmail.com>
2025-02-20 20:54:27 -0500

    Added Spring
    Created and included the Spring Boot side using the Spring initializr

---
+++ locate-app/Locate/src/main/java/com/java/Locate/LocateApplication.java
@@ -0,0 +1,13 @@
+package com.java.Locate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocateApplication.class, args);
+	}
+
+}
---
+++ locate-app/Locate/src/test/java/com/java/Locate/LocateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.Locate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
